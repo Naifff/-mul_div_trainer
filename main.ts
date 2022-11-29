1577baaff22484bf791026e5f1341a5c438df800
Naif <knifffe@yandex.ru>
2022-11-29 08:14:57 +0300

    Первоначальные файлы для проекта MakeCode

---
+++ main.ts
@@ -0,0 +1,63 @@
+input.onButtonPressed(Button.A, function () {
+    if (e == 0) {
+        if (d == 0) {
+            a = randint(1, 9)
+            b = randint(1, 99)
+            c = a * b
+            d = 1
+            basic.showString("  " + a + "*" + b + " ")
+        } else {
+            basic.showString(" " + a + "*" + b + " ")
+        }
+    } else {
+        if (d == 0) {
+            c = randint(1, 9)
+            b = randint(1, 99)
+            a = c * b
+            d = 1
+            basic.showString("  " + a + "/" + b + " ")
+        } else {
+            basic.showString(" " + a + "/" + b + " ")
+        }
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (e == 0) {
+        e = 1
+        basic.showLeds(`
+            . . . # #
+            . . # # #
+            . # # # .
+            # # # . .
+            # # . . .
+            `)
+    } else {
+        e = 0
+        basic.showLeds(`
+            # # . # #
+            # # # # #
+            . # # # .
+            # # # # #
+            # # . # #
+            `)
+    }
+    d = 0
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(c)
+    d = 0
+})
+let c = 0
+let b = 0
+let a = 0
+let e = 0
+let d = 0
+basic.showLeds(`
+    # # . # #
+    # # # # #
+    . # # # .
+    # # # # #
+    # # . # #
+    `)
+d = 0
+e = 0
